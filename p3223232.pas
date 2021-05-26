36afb40cfe7b8427fe65177d0fcfe6b10784cd7f
Даша <81682662+Darya2232323@users.noreply.github.com>
2021-05-26 10:55:14 +0300

    Add files via upload

---
+++ p3223232.pas
@@ -0,0 +1,11 @@
+﻿Program sumcifr; 
+var a, sum: integer; 
+Begin 
+  Writeln ('Введите число'); 
+  Readln (a); 
+  while a<>0 do begin 
+    sum:=sum+(a mod 10); 
+    a:=a div 10;
+  end; 
+  Writeln ('Сумма цифр: ',sum);
+end. 
